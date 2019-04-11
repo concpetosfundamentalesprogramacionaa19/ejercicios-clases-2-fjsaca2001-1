5ce67d6cae2fe1da72fd565000d348116be45b2b
fjsaca2001 <sacafrank2@gmail.com>
2019-04-11 11:06:04 -0500

    Java

--- ejemplosz2/src/ejemplosz2/ejemplosclases22.java
+++ ejemplosz2/src/ejemplosz2/ejemplosclases22.java
@@ -21,8 +21,9 @@ public class ejemplosclases22 {
         // NO OLVIDAR INGRESAR VALORES 
         
         String nombre;
-        
         String apellido;
+        int edad;
+        String ciudad;
         Scanner entrada = new Scanner(System.in);
         
         System.out.println("Ingrese sus nombres: ");
@@ -31,9 +32,16 @@ public class ejemplosclases22 {
         System.out.println("Ingrese sus Apellidos: ");
         apellido = entrada.nextLine();
         
-        System.out.println("Su nombre es: "+nombre+"\n\n\t"+"Su apellido "
-        +"es: "+apellido);
-      
+        System.out.println("Ingrese su Ciudad de origen: ");
+        ciudad = entrada.nextLine();
+        
+        System.out.println("Ingrese sus Edad: ");
+        edad = entrada.nextInt();
+        
+        
+        
+        System.out.println("Su nombre es: "+nombre+"\n\n\t"+"su apellido es: "
+                +apellido+", edad: "+edad+"\n"+"Ciudad:" +ciudad);
     }
     
 }
