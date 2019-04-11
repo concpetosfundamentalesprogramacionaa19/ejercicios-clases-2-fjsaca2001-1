8b3b43d4b2a729b276c0bf903e7d503efca7ad3e
fjsaca2001 <sacafrank2@gmail.com>
2019-04-11 11:57:19 -0500

    Java

---
+++ ejemplosz2/src/Ejercicioclase/miejercicio.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicioclase;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DELL
+ */
+public class miejercicio {
+    public static void main(String[] args) {
+        String nombre;
+        String apellido;
+        int edad;
+        String ciudad;
+        String pais;
+        float notas1,notas2;
+        float promedio;
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Ingrese sus nombres: ");
+        nombre = entrada.nextLine();
+       
+        System.out.println("Ingrese sus Apellidos: ");
+        apellido = entrada.nextLine();
+        
+        System.out.println("Ingrese sus Edad: ");
+        edad = entrada.nextInt();
+        // Limpieza de buffer
+        entrada.nextLine();
+        
+        System.out.println("Ingrese su Ciudad de origen: ");
+        ciudad = entrada.nextLine();
+        
+        System.out.println("Ingrese su Pais de origen: ");
+        pais = entrada.nextLine();
+        
+        System.out.println("Ingrese sus Nota #1: ");
+        notas1 = entrada.nextInt();
+        
+        System.out.println("Ingrese sus Nota #2: ");
+        notas2 = entrada.nextInt();
+        
+        promedio=(notas1+notas2)/2;
+   
+        System.out.printf("Sus nombres y apellidos: %s %s \nEdad: %s \n Ciudad: %s \nPais: %s \nNotas %s,%s \nPromedio: %s",nombre,apellido,edad,ciudad,pais,notas1,notas2,promedio);
+    }
+}
